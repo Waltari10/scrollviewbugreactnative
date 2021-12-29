e7f789dadf2a7377e3dc13473f6d7d64cea85c10
Runner <runner@example.com>
2021-12-29 17:37:23 +0200

    simplify

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {View} from 'react-native';
 export default () => (
   <View
     onLayout={layout => {
-      console.log('appText height', layout.nativeEvent.layout.height); // Should be 30, but says 29.81818199157715 and 30.18181800842285 for every other
+      console.log('appText height', layout.nativeEvent.layout.height); // Should be 30, but says 30.18181800842285
     }}
     style={{
       height: 30,
